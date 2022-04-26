88676d94822894e001f42c3e0fc25fb03e3b3e97
SGY3 <sgy3.sgy3@gmail.com>
2022-04-27 01:29:03 +0530

    Added Repository of Cart and Application User

--- MyApp.DataAccessLayer/Infrastructure/Repository/UnitOfWork.cs
+++ MyApp.DataAccessLayer/Infrastructure/Repository/UnitOfWork.cs
@@ -12,13 +12,17 @@ namespace MyApp.DataAccessLayer.Infrastructure.Repository
         public ICategoryRepository Category { get; private set; }
 
         public IProductRepository Product { get; private set; }
+        public ICartRepository Cart { get; private set; }
+        public IApplicationUser ApplicationUser { get; private set; }
 
         private ApplicationDbContext _context;
         public UnitOfWork(ApplicationDbContext context)
         {
             _context = context;
-            Category=new CategoryRepository(context);
-            Product=new ProductRepository(context);
+            Category = new CategoryRepository(context);
+            Product = new ProductRepository(context);
+            Cart = new CartRepository(context);
+            ApplicationUser = new ApplicationRepository(context);
         }
 
         public void Save()
--- MyApp.Models/Cart.cs
+++ MyApp.Models/Cart.cs
@@ -4,11 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MyApp.Models.ViewModels
+namespace MyApp.Models
 {
     public class Cart
     {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+
         public Product Product { get; set; }
+        public string AppliationUserId { get; set; }
+        public ApplicationUser AppliationUser { get; set; }
         public int Count { get; set; }
     }
 }
